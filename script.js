c0f98bf23692fb3f5993020062220656b2f70849
MartinKinyuka <mkinyuka@gmail.com>
2021-04-12 23:45:26 +0300

    input form for birthdate added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame" ];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ]
 
+function getAkanName(){
+  var birthDay = document.getElementById("birthDay").value;
+}
