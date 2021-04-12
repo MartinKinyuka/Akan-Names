c99eb2f9011415cc613fa7459d16931f0458744b
MartinKinyuka <mkinyuka@gmail.com>
2021-04-13 00:20:11 +0300

    conditional statement added

--- script.js
+++ script.js
@@ -3,4 +3,22 @@ var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ]
 
 function getAkanName(){
   var birthDay = document.getElementById("birthDay").value;
+  var fmle = document.getElementById("fmle");
+  var mle = document.getElementById("mle");
+  conversionFunction(birthDay,fmle,mle)
+}
+
+
+function conversionFunction(birthDay,fmle,mle){
+  var birthDate = new Date(birthDay);
+  var dayOfWeek = birthDate.getDay();
+  if(fmle.checked == true){
+    document.getElementById("yourAkanName").innerHTML = "Your Akan name is "+ femaleNames[dayOfWeek];
+  }
+  else if(mle.checked == true){
+    document.getElementById("yourAkanName").innerHTML = "Your Akan name is "+ maleNames[dayOfWeek];
+  }
+  else{
+    alert("Please fill/check all items in form")
+  }
 }
